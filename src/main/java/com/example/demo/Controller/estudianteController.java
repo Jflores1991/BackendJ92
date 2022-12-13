d40afe97b566404248395d809d24967ed680b2ba
Jflores1991 <105498045+Jflores1991@users.noreply.github.com>
2022-12-12 19:48:09 -0500

    Update estudianteController.java

--- src/main/java/com/example/demo/Controller/estudianteController.java
+++ src/main/java/com/example/demo/Controller/estudianteController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/estudiante")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://frontj92.azurewebsites.net")
 
 public class estudianteController {
 
